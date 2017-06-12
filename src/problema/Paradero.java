047fa7e81402dd4c93bdb52e1ef675b5339f9f69
GabrielGuzman05 <g.guzman05@ufromail.cl>
2017-06-12 18:59:23 -0400

    Revert "Correcion en nivel de acceso a atributos y metodos"
    This reverts commit 8ceb918033c7fb7fbd391dfaca1b00e7ec471c19.

--- src/problema/Paradero.java
+++ src/problema/Paradero.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Paradero {
 
-    private ArrayList<Linea> lineas;
+    public ArrayList<Linea> lineas;
     private String codigo;
 
     public Paradero(String c) {
