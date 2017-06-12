8ceb918033c7fb7fbd391dfaca1b00e7ec471c19
GabrielGuzman05 <g.guzman05@ufromail.cl>
2017-06-12 18:59:05 -0400

    Correcion en nivel de acceso a atributos y metodos

--- src/problema/Paradero.java
+++ src/problema/Paradero.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Paradero {
 
-    public ArrayList<Linea> lineas;
+    private ArrayList<Linea> lineas;
     private String codigo;
 
     public Paradero(String c) {
